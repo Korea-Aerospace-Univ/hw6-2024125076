1ccedae286565cabef8b023f7213e97b77f97709
2024125076 <senghon0212@gmail.com>
2026-06-01 15:13:51 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,34 +1,31 @@
 
-
 #include <stdio.h>
 
 int main()
 {
-    char arr[10]={};
-    char *p=arr;
-    char max_char=*p;
-    char current_char=*(p+1);
+    char arr[100];
+    char max_char='\0';
     int max_count=0;
     int count=0;
     
-    for(int i=0;i<10;i++){
-        scanf("%c",(p+i));
-    }
-    for(int i=0;i<10;i++){
-        current_char=*(p+1);
-        
+    
+    for(char *p=arr;p<arr+10;p++){
+        scanf("%c",p);
         
     }
-    for(int j=0;j<10;j++){
-        if(*(p+j)==current_char){
-            count ++;
+    for(char*p=arr;p<arr+10;p++){
+        count=0;
+        for(char *q=arr;q<arr+10;q++){
+            if(*q==*p){
+                count++;
+            }
+        }
+        if(count>max_count){
+        max_count=count;
+        max_char=*p;
         }
     }
-    if(count>max_count){
-        max_count=count;
-        max_char=current_char;
-        
-    }
-    printf("%c%d", max_char, max_count);
     
+    printf("%c%d",max_char,max_count);
+    return 0;
 }
